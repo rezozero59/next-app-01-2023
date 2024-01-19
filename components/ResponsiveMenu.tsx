3a02c90fe88cba2280bf5285975b8b3d1696afc0
rezozero59 <laurentdelassus@gmail.com>
2024-01-19 12:05:23 +0100

    Responsive Menu with Chadcn OK (sheet)

--- components/ResponsiveMenu.tsx
+++ components/ResponsiveMenu.tsx
@@ -2,18 +2,30 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Category } from "@/types";
+import { CATEGORIES } from "@/utils/categories";
 
 export default function ResponsiveMenu() {
   return (
     <Sheet>
       <SheetTrigger>
-        <Menu className="w-6 h-6  text-gray-500" />
+        <Menu className="w-6 h-6 md:hidden text-gray-500" />
       </SheetTrigger>
       <SheetContent side="left">
-        <div>
+        <div className="flex flex-col gap-4">
           <Link href="/write">
             <Button variant="ghost">Write A Post</Button>
           </Link>
+          <p>Catégories</p>
+          {CATEGORIES.map((category: Category) => (
+            <Link
+              key={category.id}
+              href={`/category/${category.slug}`}
+              className="block px-2 py-1 text-lg"
+            >
+              <Button variant="ghost">{category.name}</Button>
+            </Link>
+          ))}
         </div>
       </SheetContent>
     </Sheet>
