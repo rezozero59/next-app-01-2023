c1423d214991d16dc75cbd70c79f6e5aedf9d0ff
rezozero59 <laurentdelassus@gmail.com>
2024-01-21 12:16:06 +0100

    Hero Image, input email, button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+
 import Header from "@/components/Header";
 import ThemeProvider from "@/providers/theme-provider";
 import Footer from "@/components/Footer";
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import PageContainer from "@/components/PageContainer";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  const router = useRouter();
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      MAIN
-    </main>
+    <PageContainer>
+      <div className="py-10 px-4">
+        <div
+          style={{ backgroundImage: "url(/img/coding-hero.jpg)" }}
+          className="rounded-lg aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+        >
+          <div className="h-full w-full flex flex-col justify-center items-center">
+            <div className="sm:max-w-xl max-w-xs bg-secondary/80 p-4 rounded-lg">
+              <h1 className="text-center font-bold text-3xl sm:text-5xl text-black dark:text-white ">
+                Deviens un bon dev web
+              </h1>
+              <Input
+                type="email"
+                placeholder="Email"
+                className="dark:bg-white mt-4"
+              />
+              <Button size="lg" className="w-full py-6 text-xl mt-4">
+                Inscris-toi
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </PageContainer>
   );
 }
