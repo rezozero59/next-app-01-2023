87fdb299ecb5b1e1b6a74adff89bab1f3359cbaf
rezozero59 <laurentdelassus@gmail.com>
2024-01-22 19:33:20 +0100

    post-card , chadcn, static to dynamic later

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,9 +1,31 @@
 import { Post } from "@/types";
+import Link from "next/link";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import Image from "next/image";
 
 type Props = {
   post: Post;
 };
 
 export default function PostCard({ post }: Props) {
-  return <div>{post.title}</div>;
+  return (
+    <Link href={`/posts/${post.slug}`}>
+      <Card className="flex flex-col justify-between rounded-lg border-2">
+        <CardHeader>
+          <div className="aspect-square relative">
+            <Image
+              src="/img/coding-hero.jpg"
+              alt={post.title}
+              fill
+              className="aspect-square object-cover transition-all duration-300 hover:scale-110"
+            />
+          </div>
+        </CardHeader>
+
+        <CardContent></CardContent>
+
+        <CardFooter></CardFooter>
+      </Card>
+    </Link>
+  );
 }
