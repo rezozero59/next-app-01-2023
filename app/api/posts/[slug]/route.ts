6c5ccc5a3c1135d5d2a13dfadbd4e136edc650a1
rezozero59 <laurentdelassus@gmail.com>
2024-01-27 20:04:52 +0100

    test with POST (DB is coming..)

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,13 +1,31 @@
 // Get single post
 
+import { Post } from "@/types";
 import { NextResponse } from "next/server";
+
+const POST: Post = {
+  id: 1,
+  category: "React",
+  title: "React State Management: Choosing the Right Solution",
+  image: "/react-state-management.jpg",
+  caption:
+    "Explore different state management solutions in React and choose the one that fits your needs.",
+  date: "2023-01-15",
+  minutesToRead: 10,
+  author: "John ReactDev",
+  nbViews: 25,
+  nbComments: 8,
+  slug: "react-state-management-choosing-right-solution",
+  content: "lorem ipsum",
+};
 
 export const GET = async (
   req: Request,
   { params }: { params: { slug: string } }
 ) => {
+  const { slug } = params;
   // api/posts/react-native
   // slug en params
   // -->DB -->post
-  return NextResponse.json(params.slug, { status: 200 });
+  return NextResponse.json(POST, { status: 200 });
 };
