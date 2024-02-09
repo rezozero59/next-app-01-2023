67311c069dcdab45c5d3604064f8c5e234ae2067
rezozero59 <laurentdelassus@gmail.com>
2024-02-09 17:46:03 +0100

    usePosts debug ok

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -2,8 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const usePosts = () => {
-  return useQuery("posts", async () => {
-    const { data } = await axios.get(`/api/posts`);
-    return data;
+  return useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/posts`);
+      return data;
+    },
   });
 };
