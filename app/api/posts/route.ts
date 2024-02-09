aad43ad10f284a55d9fb281e20410cebb9e53b0e
rezozero59 <laurentdelassus@gmail.com>
2024-02-09 17:55:20 +0100

    Debug done

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    const posts = prisma.post.findMany();
+    const posts = await prisma.post.findMany();
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {}
   return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
