0d34818e7d389ac53a6722230907abd6dbea789d
rezozero59 <laurentdelassus@gmail.com>
2024-02-14 20:36:40 +0100

    start next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 import ThemeProvider from "@/providers/theme-provider";
 import Footer from "@/components/Footer";
 import QueryProvider from "@/providers/query-provider";
+import AuthProvider from "@/providers/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,16 +24,18 @@ export default function RootLayout({
     <html lang="fr">
       <body className={inter.className}>
         <QueryProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div
-              className="flex flex-col 
+          <AuthProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <div
+                className="flex flex-col 
          justify-between flex-fill min-h-screen"
-            >
-              <Header />
-              <div className="flex-grow">{children}</div>
-              <Footer />
-            </div>
-          </ThemeProvider>
+              >
+                <Header />
+                <div className="flex-grow">{children}</div>
+                <Footer />
+              </div>
+            </ThemeProvider>
+          </AuthProvider>
         </QueryProvider>
       </body>
     </html>
