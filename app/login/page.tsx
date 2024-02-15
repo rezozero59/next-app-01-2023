64224fafacd75866fe809fe94862f83a9fcc8d8a
rezozero59 <laurentdelassus@gmail.com>
2024-02-15 19:41:42 +0100

    login with google

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,26 @@
+"use client";
+
 import PageContainer from "@/components/PageContainer";
 import PageTitle from "@/components/page-title";
 import { Button } from "@/components/ui/button";
 import { Github, Mail } from "lucide-react";
+import { signIn } from "next-auth/react";
 import React from "react";
 
 export default function LoginPage() {
+  const onLogin = (provider: string) => () => {
+    signIn(provider);
+  };
   return (
     <PageContainer>
       <div className="p-10">
         <PageTitle title="Login or register" />
         <div className="flex flex-col gap-4 max-w-sm mx-auto ">
-          <Button>
+          <Button onClick={onLogin("github")}>
             <Github className="mr-3" />
             Signin with Github
           </Button>
-          <Button>
+          <Button onClick={onLogin("google")}>
             <Mail className="mr-3" />
             Signin with Google
           </Button>
