6e2f1df78be30cb3253f014e81b18631e999e6f4
rezozero59 <laurentdelassus@gmail.com>
2024-02-16 19:04:07 +0100

    write page to private route

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function WritePage() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  if (!session) {
+    router.replace("/login");
+  }
+
   return <div>WritePage</div>;
 }
